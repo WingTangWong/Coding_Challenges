4c51e3d10a2acd5b60a80e96c061bd5d25a3e067
Wing Wong <wingedpower@gmail.com>
2021-12-06 01:51:00 -0800

    AOC 2020 5a

--- AdventOfCoding/2020/05/README.md
+++ AdventOfCoding/2020/05/README.md
@@ -1,12 +1,30 @@
-# FUN NAME FOR THE CODE CHALLENGE
+# Airplane Boarding Pass... Seat Decoder
 
-[AOC 2020](https://adventofcode.com/2020/day/DAY)
+[AOC 2020](https://adventofcode.com/2020/day/5)
 
 SUMMARY
 
+Decode boarding passes...
+
+  BFFFBBFRRR: row 70, column 7, seat ID 567.
+  FFFBBBFRRR: row 14, column 7, seat ID 119.
+  BBFFBBFRLL: row 102, column 4, seat ID 820.
+
 ## 1st half
 
-PROBLEMDESC1
+128 rows of seats in the plane...
+
+For example, consider just the first seven characters of FBFBBFFRLR:
+
+Start by considering the whole range, rows 0 through 127.
+F means to take the lower half, keeping rows 0 through 63.
+B means to take the upper half, keeping rows 32 through 63.
+F means to take the lower half, keeping rows 32 through 47.
+B means to take the upper half, keeping rows 40 through 47.
+B keeps rows 44 through 47.
+F keeps rows 44 through 45.
+The final F keeps the lower of the two, row 44.
+
 
 ## 2nd half
 
--- AdventOfCoding/2020/05/solution-05a.rb
+++ AdventOfCoding/2020/05/solution-05a.rb
@@ -3,5 +3,40 @@
 SRC     = ARGV[0]
 fh      = File.open(SRC)
 rawdata = fh.readlines.map { | datum | datum.chomp }
+max_seat_id=0
+for datum in rawdata do
+  entry = datum.split(/:/)[0].to_s
+  bit_pos=0
+  row_bit_value="0000000"
+  col_bit_value="000"
+  for c in datum.each_char.map do
+    bit=0
+    if c == "F" then
+      bit = 0
+    end
+    if c == "B" then
+      bit = 1
+    end
+    if c == "L" then
+      bit = 0
+    end
+    if c == "R" then
+      bit = 1
+    end
+    if bit_pos <= 6 then
+      row_bit_value[bit_pos]=bit.to_s
+    else
+      col_bit_value[bit_pos - 6]=bit.to_s
+    end
+    bit_pos += 1
+  end
+  seat_row = row_bit_value.to_i(2)
+  seat_col = col_bit_value.to_i(2)
+  seat_id  = ( seat_row * 8 ) + seat_col
+  if seat_id > max_seat_id then
+    max_seat_id = seat_id
+  end
+  puts "[ %s ] => [ ROW  %5d ] [ COL %3d ] => SEAT ID [ %3d ]" % [ datum , seat_row, seat_col , seat_id ]
+end
 
-
+puts max_seat_id
