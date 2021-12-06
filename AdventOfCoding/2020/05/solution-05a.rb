c80091c41150a33b271a77ea54185eadbc10a6f0
Wing Wong <wingedpower@gmail.com>
2021-12-06 11:06:46 -0800

    Updated 2021-5a with better conversion algo

--- AdventOfCoding/2020/05/solution-05a.rb
+++ AdventOfCoding/2020/05/solution-05a.rb
@@ -6,30 +6,35 @@ rawdata = fh.readlines.map { | datum | datum.chomp }
 max_seat_id=0
 for datum in rawdata do
   entry = datum.split(/:/)[0].to_s
-  bit_pos=0
-  row_bit_value="0000000"
-  col_bit_value="000"
-  for c in datum.each_char.map do
-    bit=0
-    if c == "F" then
-      bit = 0
-    end
-    if c == "B" then
-      bit = 1
-    end
-    if c == "L" then
-      bit = 0
-    end
-    if c == "R" then
-      bit = 1
-    end
-    if bit_pos <= 6 then
-      row_bit_value[bit_pos]=bit.to_s
-    else
-      col_bit_value[bit_pos - 6]=bit.to_s
-    end
-    bit_pos += 1
-  end
+
+  # Simpler and faster method
+  entry_bit_value=entry.gsub("B","1").gsub("F","0").gsub("R","1").gsub("L","0")
+  row_bit_value = entry_bit_value[0..6]
+  col_bit_value = entry_bit_value[7..9]
+#    bit_pos=0
+#    row_bit_value="0000000"
+#    col_bit_value="000"
+#    for c in datum.each_char.map do
+#      bit=0
+#      if c == "F" then
+#        bit = 0
+#      end
+#      if c == "B" then
+#        bit = 1
+#      end
+#      if c == "L" then
+#        bit = 0
+#      end
+#      if c == "R" then
+#        bit = 1
+#      end
+#      if bit_pos <= 6 then
+#        row_bit_value[bit_pos]=bit.to_s
+#      else
+#        col_bit_value[bit_pos - 6]=bit.to_s
+#      end
+#      bit_pos += 1
+#    end
   seat_row = row_bit_value.to_i(2)
   seat_col = col_bit_value.to_i(2)
   seat_id  = ( seat_row * 8 ) + seat_col
