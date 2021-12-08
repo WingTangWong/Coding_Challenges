781c3990779016bbbf594a075e0829aa5955941c
Wing Wong <wingedpower@gmail.com>
2021-12-08 09:17:33 -0800

    Adding 2020-06-pre-solutions

--- AdventOfCoding/2020/06/solution-06b.rb
+++ AdventOfCoding/2020/06/solution-06b.rb
@@ -2,6 +2,59 @@
 
 SRC     = ARGV[0]
 fh      = File.open(SRC)
-rawdata = fh.readlines.map { | datum | datum.chomp }
 
+rawdata = fh.readlines.map(&:chomp)
+
+didx=0
+gidx=0
+
+
+datum=""
+while didx <= rawdata.length do
+  datum += rawdata[didx] + ":" 
+
+end
+
+
+
+
+groups=rawdata.to_a.map { |x| }
+
+
+gidx=0
+
+puts "#{groups}"
+
+exit 
+
+# Prep the hash
+for i in 0.upto(rawdata.length+1) do
+  groups[i]=[]
+end
+
+
+def group_vote_count(group)
+  tally=0
+  voters=group.length
+  votes=group.map { |v| v.split(//) }
+
+  common_votes=votes[0]
+
+  votes.map { |v| common_votes &= v }
+
+  puts "#{group.length} #{common_votes}"
+  puts ""
+end
+
+for dline in rawdata do
+  if dline != "" then
+    groups[gidx].append(dline)
+  else
+    group_vote_count(groups[gidx])
+    gidx += 1 
+  end
+end
+
+gidx += 1 
+groups[gidx].append(dline)
 
